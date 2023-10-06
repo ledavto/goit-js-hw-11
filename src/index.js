0cc3d3daa236e4d384a53f7380a38b2136bf75c8
ledavto <ser_ega@ukr.net>
2023-10-06 14:56:19 +0300

    update

--- src/index.js
+++ src/index.js
@@ -8,22 +8,31 @@
 
 import Notiflix from 'notiflix';
 import axios from 'axios';
-import { galleryItems } from './gallery-items.js';
+
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY_API = '39839369-8c713c9a2c0ac40d1d76da13a';
+const IMG_KOL = 40;
+let pageNum = 1;
 
 const elForm = document.querySelector('.search-form');
+const elBtn = document.querySelector('.load-more');
 const elGallery = document.querySelector('.gallery');
 
 elForm.addEventListener('submit', onClickSearch);
+elBtn.addEventListener('click', onLoadMore);
 
 //Объвление асинхронной функции
-const fetchSearchPhoto = async input => {
+const fetchSearchPhoto = async (input, page) => {
+  console.log(page);
   const response = await axios.get(
-    `${BASE_URL}?key=${KEY_API}&q=${input}&image_type=photo`
+    `${BASE_URL}?key=${KEY_API}&q=${input}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${IMG_KOL}&page=${page}`
   );
   const photos = await response.data.hits;
+  const totalImg = await console.log(response.data.totalHits);
   return (photoCard = photos
     .map(photo => {
       return `<div class="photo-card">
@@ -59,7 +68,31 @@ function onClickSearch(event) {
   const input = form.elements.searchQuery.value;
   //console.log(input);
 
-  fetchSearchPhoto(input).then(photos =>
+  if (document.querySelectorAll('.photo-card').length > 0)
+    for (const variable of document.querySelectorAll('.photo-card')) {
+      // тіло циклу
+      variable.remove();
+    }
+
+  localStorage.setItem('gallery-input-str', input);
+  localStorage.setItem('gallery-page-num', 1);
+
+  fetchSearchPhoto(input, pageNum).then(photos =>
+    elGallery.insertAdjacentHTML('beforeend', photos)
+  );
+}
+
+function onLoadMore(event) {
+  //event.preventDefault();
+  //const form = event.currentTarget;
+  const inputText = document.querySelector('input');
+
+  console.log(inputText.value);
+  pageNum = localStorage.getItem('gallery-page-num');
+  console.log(pageNum++);
+  localStorage.setItem('gallery-page-num', pageNum);
+
+  fetchSearchPhoto(inputText, pageNum++).then(photos =>
     elGallery.insertAdjacentHTML('beforeend', photos)
   );
 }
